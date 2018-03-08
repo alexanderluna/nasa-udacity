21d861fba213d259ac8de253836e3a5619559471
Mycroft1891 <mycroft1891@gmail.com>
2018-03-08 16:30:34 +0100

    replace react-youtube with react-player

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,30 +1,24 @@
 import React, { Component } from 'react';
 import { CardMedia } from 'material-ui/Card';
-import Youtube from 'react-youtube';
+import ReactPlayer from 'react-player'
 
 class Media extends Component {
-  getVideoID() {
-    if (this.props.url.match("embed/")) {
-      return this.props.url.split("embed/")[1].split("?")[0]
-    }
-  }
 
   render() {
-    const { type, url, title } = this.props;
-
-    return(
-      <div>
-        { (type === 'video')
-          ? <CardMedia className="card-media">
-              <Youtube videoId={ this.getVideoID() } />
-            </CardMedia>
-          : <CardMedia className="card-media">
-              <img src={ url } alt={ title } />
-            </CardMedia>
-        }
-      </div>
-    )
-  }
+		const { type, url, title } = this.props;
+		return(
+			<div>
+				{ (type === 'video')
+				? <CardMedia className="card-media">
+						<ReactPlayer url={url} controls={true}/>
+					</CardMedia>
+				: <CardMedia className="card-media">
+						<img src={ url } alt={ title } />
+					</CardMedia>
+				}
+			</div>
+		)
+	}
 }
 
 export default Media
