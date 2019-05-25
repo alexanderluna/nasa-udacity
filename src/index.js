600c5425d64a2c1f0ff28e70d94f3048e1e68709
Mycroft1891 <mycroft1891@gmail.com>
2019-05-25 23:55:06 +0200

    Update: clean up whitespace

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React      from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import App        from './components/App';
+import App from './components/App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
