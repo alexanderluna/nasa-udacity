8cc6a38b46db499e8668e0f1b503eeb7aa83fa61
Mycroft1891 <mycroft1891@gmail.com>
2019-06-23 00:42:30 +0200

    Update: use grid system, internalize styling and export API requests

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,56 +1,69 @@
 import React, { Component } from 'react';
+import { Grid } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import getImageList from '../services';
 import Image from './Image';
 import Navigation from './Navigation';
 import fire from '../fire';
 
+const styles = {
+  root: {
+    flexGrow: 1,
+    marginTop: '4rem',
+  },
+  item: {
+    maxHeight: 300,
+  },
+  media: {
+    borderWidth: 0,
+    height: '100%',
+  },
+};
+
+
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { images: [], detail: true }
+    this.state = { images: [], detail: true };
   }
 
-  componentWillMount() {
+  componentDidMount = async () => {
     if (this.props.match.params.id) {
       this.setState({ detail: false });
     }
-    const imagesRef = fire.database().ref('Images');
-    imagesRef.on('child_added', (image) => {
-      const data = image.val();
-      data.id = image.ref.key;
-      this.setState((prev) => ({
-        images: [data].concat(prev.images),
-      }));
-    });
+    const imageList = await getImageList();
+    this.setState({ imageList });
   }
 
-  nextPage = (page) => page ? parseInt(page, 10) + 1 : 2;
+  nextPage = (page) => page ? parseInt(page, 20) + 1 : 2;
 
-  previousPage = (page) => (page && page > 1) ? parseInt(page, 10) - 1 : 1;
-
-  sortByDate() {
-    return this.state.images.sort((first, second) => {
-      if (first.date < second.date) { return -1; }
-      if (first.date > second.date) { return 1; }
-      return 0;
-    });
-  }
+  previousPage = (page) => (page && page > 1) ? parseInt(page, 20) - 1 : 1;
 
   filterByParams(id, page) {
     if (id) {
       return this.state.images.filter((img) => (img.id === id) ? img : false);
     }
-    const end = page ? page * 10 : 10;
-    const start = end > 10 ? end - 10 : 0;
+    const end = page ? page * 20 : 20;
+    const start = end > 20 ? end - 20 : 0;
     return this.sortByDate().reverse().slice(start, end);
   }
 
   render() {
     const { id, page } = this.props.match.params;
+    const { classes } = this.props;
     return (
-      <div className="image-list">
-        {this.filterByParams(id, page).map((img, i) => (
-          <Image info={img} key={i} />
-        ))}
+      <div className="">
+        <Grid className={classes.root} container>
+          {this.state.images.map((img, i) => (
+            <Grid key={i} item xs={12} sm={6} md={3} className={classes.item}>
+              {/* <Image info={img} /> */}
+              {img.media_type === 'image'
+                ? <img src={img.url} alt={img.title} />
+                : <iframe src={img.url} title={img.title} className={classes.media} />
+              }
+            </Grid>
+          ))}
+        </Grid>
         {this.state.detail && (
           <Navigation
             prev={this.previousPage(page)}
@@ -62,4 +75,4 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
