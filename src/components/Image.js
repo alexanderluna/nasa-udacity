60dcec9e842bf9601aa87032df5145edd6763dcb
Mycroft1891 <mycroft1891@gmail.com>
2019-06-24 00:43:11 +0200

    Update: internalize styling

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,47 +2,60 @@ import React from 'react';
 import {
   Avatar, Button, Card, CardHeader, CardContent, Typography,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import Media from './Media';
 import nasa from '../assets/nasa.png';
 
+const useStyles = makeStyles(() => ({
+  cardPaper: {
+    maxHeight: 600,
+  },
+  image: {
+    width: '100%',
+  },
+}));
 
-const Image = ({ info }) => (
-  <Card className="card">
-    <CardHeader
-      title={`Image Credit: ${info.copyright}`}
-      subheader={info.date}
-      avatar={(
-        <Avatar
-          aria-label="nasa iod"
-          src={nasa}
-          alt="nasa icon"
-        />
-      )}
-    />
-    <Media
-      type={info.media_type}
-      url={info.url}
-      title={info.title}
-    />
-    <CardContent>
-      <Button
-        fullWidth
-        variant="contained"
-        color="secondary"
-      >
-        <Link to={`/image/${info.id}`}>View Image</Link>
-      </Button>
-      <Typography
-        className="card-text"
-        variant="body2"
-        color="textSecondary"
-        component="p"
-      >
-        {info.explanation}
-      </Typography>
-    </CardContent>
-  </Card>
-);
+const Image = ({ info }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.cardPaper}>
+      <CardHeader
+        title={`Credit: ${info.copyright}`}
+        subheader={info.date}
+        avatar={(
+          <Avatar
+            aria-label="nasa iod"
+            src={nasa}
+            alt="nasa icon"
+          />
+        )}
+      />
+      <Media
+        type={info.media_type}
+        url={info.url}
+        title={info.title}
+      />
+      <CardContent>
+        <Button
+          fullWidth
+          variant="contained"
+          color="secondary"
+        >
+          <Link to={`/image/${info.id}`}>View Image</Link>
+        </Button>
+        <Typography
+          className="card-text"
+          variant="body2"
+          color="textSecondary"
+          component="p"
+        >
+          {info.explanation}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default Image;
