09f3b7758420f285ea74f4d404e34687a0bcdc90
Mycroft1891 <mycroft1891@gmail.com>
2019-07-05 18:14:48 +0200

    Update: setup redux store with redux thunk

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
 import './index.css';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+import App from './components/App';
 
-render(<App />, document.querySelector('#root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root'),
+);
+
 registerServiceWorker();
